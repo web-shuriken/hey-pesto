1d78994d0b0eab08ac6b390aefe0a7cb7ef7baf5
Steven Cheung <stevencheung@hotmail.co.uk>
2022-09-09 11:47:47 +0100

    completed homepage

--- src/components/HomePage/App.js
+++ src/components/HomePage/App.js
@@ -1,10 +1,17 @@
-import Header from '../Header/Header';
-import './App.css';
+import Header from "../Header/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <img
+        src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?resize=960,872?quality=90&resize=556,505"
+        alt="home-picture"
+      />
+      <div className="home-text">
+        <p>Hey l'ottimo cibo!</p>
+      </div>
     </div>
   );
 }
