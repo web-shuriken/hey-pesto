ae40067ca9a82e1bc4119b27c27ac28758253106
Steven Cheung <stevencheung@hotmail.co.uk>
2022-09-09 12:07:14 +0100

    minor changes

--- src/components/HomePage/App.js
+++ src/components/HomePage/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <img
-        src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?resize=960,872?quality=90&resize=556,505"
-        alt="home-picture"
-      />
-      <div className="home-text">
-        <p>Hey l'ottimo cibo!</p>
-        <button>View Menu</button>
+      <div className="homepage-container">
+        <img
+          src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?resize=960,872?quality=90&resize=556,505"
+          alt="home-picture"
+        />
+        <div className="home-text">
+          <p>Hey l'ottimo cibo!</p>
+          <button>View Menu</button>
+        </div>
       </div>
     </div>
   );
