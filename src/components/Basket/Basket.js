7d2eb3d2764d032aba195016a51e63aefb5cfea2
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-09-09 10:28:10 +0100

    Setup initial Basket and Menu components

---
+++ src/components/Basket/Basket.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Basket() {
+  return (
+    <section>
+      <h1>Cart</h1>
+      <p>cart information goes here</p>
+    </section>
+  )
+}
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <section>
+      <h1>Menu</h1>
+      <p>Food cards go here</p>
+    </section>
+  )
+}
